9adefc69c4899624ef9bec99f19ca1770e1f538c
AlbinOdelstav <albin.odelstav@outlook.com>
2021-11-20 02:59:59 +0100

    some filters added

--- Separable filters/kernel.py
+++ Separable filters/kernel.py
@@ -1,23 +1,37 @@
 import numpy
 
-
 class Kernel:
-    def __init__(self, n, size):
-        self.n = n
-        self.width = size
-        self.height = size
-        self.matrix = numpy.full((self.width, self.height), self.n)
+    sharpen = numpy.array([[0.0, -1.0, 0.0],
+                           [-1.0, 5.0, -1.0],
+                           [0.0, -1.0, 0.0]])
 
-    def calculate(self, img, new_img, x, y):
-        img_width = img.shape[0]
-        img_height = img.shape[1]
-        kernel_width_half = int(self.width / 2)
-        kernel_height_half = int(self.height / 2)
+    blur_5 = numpy.array([[1.0, 1.0, 1.0, 1.0, 1.0],
+                          [1.0, 1.0, 1.0, 1.0, 1.0],
+                          [1.0, 1.0, 1.0, 1.0, 1.0],
+                          [1.0, 1.0, 1.0, 1.0, 1.0],
+                          [1.0, 1.0, 1.0, 1.0, 1.0]])
 
-        for kernel_y in range(-kernel_height_half, kernel_height_half):
-            for kernel_x in range(-kernel_width_half, kernel_width_half):
-                kernel_offset_y = kernel_y + y
-                kernel_offset_x = kernel_x + x
-                if 0 <= kernel_offset_y < img_height and 0 <= kernel_offset_x < img_width:
-                    new_img[x, y] = img[kernel_offset_x, kernel_offset_y] * self.matrix[kernel_x, kernel_y]
-        return new_img
+    blur_3 = numpy.array([[1.0, 1.0, 1.0],
+                                      [1.0, 1.0, 1.0],
+                                      [1.0, 1.0, 1.0]])
+
+    sobel = numpy.array([[-1.0, 0.0, 1.0],
+                         [-2.0, 0.0, 2.0],
+                         [-1.0, 0.0, 1.0]])
+
+    emboss = numpy.array([[-2.0, 1.0, 0.0],
+                          [-1.0, 1.0, 1.0],
+                          [0.0, 1.0, 2.0]])
+
+    blur_5_1d = numpy.array([[1.0, 1.0, 1.0, 1.0, 1.0]])
+
+    blur_3_1d = 1.0 / 3.0 * numpy.array([[1.0, 1.0, 1.0]])
+
+    sobel_1d = numpy.array([[-1.0, 0.0, 1.0]])
+    sobel_1d_2 = numpy.array([[1.0, 2.0, 1.0]])
+
+    gaussian = numpy.array([[1.0, 4.0, 6.0, 4.0, 1.0],
+                                 [4.0, 16.0, 24.0, 16.0, 4.0],
+                                 [6.0, 24.0, 36.0, 24.0, 6.0],
+                                 [4.0, 16.0, 24.0, 16.0, 4.0],
+                                 [1.0, 4.0, 6.0, 4.0, 1.0]])
