5b20fe127fb595bb386ffc8862da76113c41497c
AlbinOdelstav <albin.odelstav@outlook.com>
2021-12-09 21:36:20 +0100

    Corrected some funny mistakes

--- Feature detection/orb.py
+++ Feature detection/orb.py
@@ -1,13 +1,10 @@
-import numpy as np
 import cv2
 
-# from matplotlib import pyplot as plt
-
-img = cv2.imread('chess.png', 0)
+img = cv2.imread('../images/img_right.jpg', 0)
 
 orb = cv2.ORB_create()
 kp = orb.detect(img, None)
 kp, des = orb.compute(img, kp)
-img2 = cv2.drawKeypoints(img, kp, None, color=(0, 255, 0), flags=0)
+img2 = cv2.drawKeypoints(img, kp, None, color=(0, 0, 255), flags=0)
 
-cv2.imwrite('chess_orb.png', img2)
+cv2.imwrite('img_right_orb.png', img2)
